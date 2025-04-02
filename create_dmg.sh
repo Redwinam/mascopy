6311f61660f1bafba64bf7adc4cdc3db0be5b153
千啾略 <redwinam@gmail.com>
2025-04-02 23:16:12 +0800

    更新create_dmg.sh脚本，将DMG镜像的名称从“MasCopy安装器”更改为“MasCopy Installer”，以统一语言风格并提升可读性。

--- create_dmg.sh
+++ create_dmg.sh
@@ -5,7 +5,7 @@ brew list create-dmg || brew install create-dmg
 
 # 为应用创建DMG镜像
 create-dmg \
-  --volname "MasCopy安装器" \
+  --volname "MasCopy Installer" \
   --volicon "mascopy.icns" \
   --window-pos 200 120 \
   --window-size 800 400 \
@@ -13,7 +13,7 @@ create-dmg \
   --icon "MasCopy.app" 200 190 \
   --hide-extension "MasCopy.app" \
   --app-drop-link 600 185 \
-  "MasCopy安装器.dmg" \
+  "MasCopy Installer.dmg" \
   "dist/MasCopy.app"
 
-echo "DMG安装包已创建: MasCopy安装器.dmg" 
+echo "DMG安装包已创建: MasCopy Installer.dmg" 
